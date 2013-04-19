353691e1ca7f3c34ac2d7b697e0dc2c34c50a61f
NathanSweet <nathan.sweet@gmail.com>
2013-04-19 16:46:16 +0200

    Premultiplied alpha

--- engine/source/2d/sceneobject/SkeletonObject.cc
+++ engine/source/2d/sceneobject/SkeletonObject.cc
@@ -238,6 +238,8 @@ void SkeletonObject::updateComposition( const F32 time )
     StringTableEntry assetId = (*mSkeletonAsset).mImageAsset.getAssetId();
 
 	 clearSprites();
+	 setSpriteSrcBlendFactor(GL_ONE);
+	 setSpriteDstBlendFactor(GL_ONE_MINUS_SRC_ALPHA);
 
 	 Vector2 vertices[4];
     for (int i = 0; i < mSkeleton->slotCount; ++i) {
