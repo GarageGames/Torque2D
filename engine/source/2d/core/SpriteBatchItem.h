df712056fa91ff2311b193761672d778f29d5242
capnlove <capnlove@gmail.com>
2013-05-26 22:40:36 -0400

    Final CompositeSprite Fix. PickPoint, PickArea and PickRay now work in all scenarios, regardless of CompositeSprite position, rotation or camera zoom level.

--- engine/source/2d/core/SpriteBatchItem.h
+++ engine/source/2d/core/SpriteBatchItem.h
@@ -291,6 +291,7 @@ public:
 
     virtual void copyTo( SpriteBatchItem* pSpriteBatchItem ) const;
 
+    inline const Vector2* getLocalOOBB( void ) const { return mLocalOOBB; }
     inline const Vector2* getRenderOOBB( void ) const { return mRenderOOBB; }
 
     void prepareRender( SceneRenderRequest* pSceneRenderRequest, const U32 batchTransformId );
