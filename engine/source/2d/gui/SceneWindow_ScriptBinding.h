d22013e1d61d4a4167116a1b02f7a9af15155fcd
Anthony J. Bentley <anthony@anjbe.name>
2015-12-07 20:48:22 -0700

    Remove stray byte order marks from C source files.

--- engine/source/2d/gui/SceneWindow_ScriptBinding.h
+++ engine/source/2d/gui/SceneWindow_ScriptBinding.h
@@ -1,4 +1,4 @@
-﻿//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 // Copyright (c) 2013 GarageGames, LLC
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
