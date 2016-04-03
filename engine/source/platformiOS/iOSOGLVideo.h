89884c1208707771eacc76513cd06ce502adbb9c
Peter Robinson <peter@circuithive.com>
2016-04-03 00:43:58 -0400

    iOS Fixes
    Included the TextSprite and made changes to fix bugs for iOS.

--- engine/source/platformiOS/iOSOGLVideo.h
+++ engine/source/platformiOS/iOSOGLVideo.h
@@ -54,6 +54,7 @@ public:
     const char* getDriverInfo();
     bool getGammaCorrection(F32 &g);
     bool setGammaCorrection(F32 g);
+    bool getVerticalSync( );
     bool setVerticalSync( bool on );
 };
 
